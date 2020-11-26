8b9b5084bd75395a3ad985c2e4c2f6b8744154ca
Thayane <thayanecarvalho3@gmail.com>
2020-11-26 12:17:05 -0300

    Removendo o que não é necessário estar no principal

--- Thayane/Projeto anterior/JavaScript_Cursos/Conhecendo o browser e padroes de projeto/client/js/app/views/MensagemView.js
+++
@@ -1,9 +0,0 @@
-class MensagemView extends View{
-    constructor(elemento) {
-        super(elemento);
-    }
-
-    _template(model){
-        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
-    }
-}
