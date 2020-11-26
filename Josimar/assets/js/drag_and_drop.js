582134012bad27046f5bfed6b2a8db865602ffab
Josimar Santos <73262930+josimarsts@users.noreply.github.com>
2020-11-26 06:59:09 -0300

    Update do layout People
    Alterações consideráveis para serem analisadas no layout

---
+++ Josimar/assets/js/drag_and_drop.js
@@ -0,0 +1,75 @@
+const activity = document.querySelectorAll('.activity');
+const card_body = document.querySelectorAll('.activities');
+
+let firstCard = null;
+let selectedCard = null; let 
+draggedActivity = null;
+
+//Percorre / busca todas as divs com classes de ".activity"
+for(let i = 0; i < activity.length; i++){
+
+	const a = activity[i];	
+
+	a.addEventListener('dragstart', function (){
+
+		draggedActivity = this;			
+		setTimeout(function(){
+			this.style.display = 'none';
+		}, 0);	
+			
+	});
+
+	a.addEventListener('dragend', function (){
+
+		setTimeout(function(){						
+			draggedActivity.style = 'block';
+			draggedActivity = null;
+		}, 0);		
+
+	});
+
+	//Percorre / busca todas as divs com classes de ".card_body"
+	for(let j = 0; j < card_body.length; j++){
+		const cb = card_body[j];
+
+		cb.addEventListener('dragstart', function (){
+			firstCard = this;
+		});
+
+		cb.addEventListener('dragover', function(e){
+			e.preventDefault();			
+		});
+
+		cb.addEventListener('dragenter', function(e){
+			e.preventDefault();
+		});
+
+		cb.addEventListener('drop', function(e){
+			this.appendChild(draggedActivity);
+			selectedCard = this;
+			/* Envia informações do "card" para a função referentes 
+			ao estilo antigo, ao estilo atual e qual "atividade" irá alterar */
+			color_activity(selectedCard, firstCard, draggedActivity);
+		});
+	}
+
+}
+// Remove e adiciona novo estilo de cor conforme o "card" selecionado
+function color_activity(s, f, a){
+	switch (s.id) {//Card selecionado (to_do, in_progress, done)
+		case "to_do": //Caso to_do
+			a.classList.remove("activity_" + f.id + "");//Remove classe antiga
+			a.classList.add("activity_to_do");//Adiciona nova classe
+			break;
+		case "in_progress"://Caso in_progress
+			a.classList.remove("activity_" + f.id + "");//Remove classe antiga
+			a.classList.add("activity_in_progress"); //Adiciona nova classe
+			break;
+		case "done"://Caso done
+			a.classList.remove("activity_to_" + f.id + "");//Remove classe antiga
+			a.classList.add("activity_done");//Adiciona nova classe
+			break;
+		default:
+			break;
+	}
+}
