cfc5156e74e7f7c5ac1182bbcbe01cc5104b513c
Thayane <thayanecarvalho3@gmail.com>
2020-12-01 14:24:10 -0300

    Tituloa dos quadros fixos

--- Thayane/assets/JS/MudarClasseResponsivo.js
+++ Thayane/assets/JS/MudarClasseResponsivo.js
@@ -4,13 +4,13 @@ var contato2 = document.querySelector('#contato2');
 document.body.onresize = function(){
     if(document.body.clientWidth >= 576)
     {
-        contato1.className = 'input-group';
-        contato2.className = 'input-group';
+        contato1.className = 'input-group mb-2';
+        contato2.className = 'input-group mb-2';
     }
 
     else
     {
-        contato1.className = 'form-group';
-        contato2.className = 'form-group';
+        contato1.className = 'form-group mb-2';
+        contato2.className = 'form-group mb-2';
     }
 }
