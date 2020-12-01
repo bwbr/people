1652cd1f5a1bcd3cc0476fe9142204dbc7966111
Thayane <thayanecarvalho3@gmail.com>
2020-12-01 07:21:45 -0300

    Conseguindo adicionar mais de uma tarefa
    Ainda não estou salvando

--- Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
+++ Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
@@ -1,20 +1,24 @@
 var campos = document.querySelector('[data-form-formacao]');
 var tarefas = document.querySelector('#accordion_fazer');
 
-document.querySelector('[data-form]').addEventListener('submit', (event) => {
+document.querySelector('[data-form-btn]').addEventListener('click', (event) => {
     event.preventDefault();
-    var formacao = '<div class="card formacoes" data-formacao draggable="true">'
-        formacao += '<div class="card-header">'
-            formacao += '<a class="card-link" data-toggle="collapse" href="#collapse1">' 
-                formacao += campos.value;
-            formacao += '</a>'
-        formacao += '</div>'
-        formacao += '<div id="collapse1" class="collapse" data-parent="#accordion_fazer">'
-            formacao += '<div class="card-body">'
-                formacao+= 'Lorem ipsum...'
+    console.log('Fui clicado')
+    var z = document.createElement('div');
+    if(campos != null){
+        let formacao = '<div class="card formacoes" data-formacao draggable="true">'
+            formacao += '<div class="card-header">'
+                formacao += '<a class="card-link" data-toggle="collapse" href="#collapse1">' 
+                    formacao += campos.value;
+                formacao += '</a>'
+            formacao += '</div>'
+            formacao += '<div id="collapse1" class="collapse" data-parent="#accordion_fazer">'
+                formacao += '<div class="card-body">'
+                    formacao+= 'Lorem ipsum...'
+                formacao += '</div>'
             formacao += '</div>'
         formacao += '</div>'
-    formacao += '</div>'
-    
-    tarefas.innerHTML += formacao;
-}); 
+        
+        z.innerHTML += formacao;
+        tarefas.appendChild(z);
+}}); 
