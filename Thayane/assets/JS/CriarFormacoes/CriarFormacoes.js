9a9852d9d2d8197216eb88fc1ded49cc7b14e8b9
Thayane <thayanecarvalho3@gmail.com>
2020-12-01 07:52:51 -0300

    Concatenando melhor

--- Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
+++ Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
@@ -1,23 +1,24 @@
 var campos = document.querySelector('[data-form-formacao]');
 var tarefas = document.querySelector('#accordion_fazer');
-
-document.querySelector('[data-form-btn]').addEventListener('click', (event) => {
+var $ = document.querySelector.bind(document);
+$('[data-form-btn]').addEventListener('click', (event) => {
     event.preventDefault();
     console.log('Fui clicado')
     var z = document.createElement('div');
     if(campos != null){
-        let formacao = '<div class="card formacoes" data-formacao draggable="true">'
-            formacao += '<div class="card-header">'
-                formacao += '<a class="card-link" data-toggle="collapse" href="#collapse1">' 
-                    formacao += campos.value;
-                formacao += '</a>'
-            formacao += '</div>'
-            formacao += '<div id="collapse1" class="collapse" data-parent="#accordion_fazer">'
-                formacao += '<div class="card-body">'
-                    formacao+= 'Lorem ipsum...'
-                formacao += '</div>'
-            formacao += '</div>'
-        formacao += '</div>'
+        let formacao = 
+        `<div class="card formacoes" data-formacao draggable="true">
+            <div class="card-header">
+                <a class="card-link" data-toggle="collapse" href="#collapse1">
+                   ${campos.value}
+                </a>
+            </div>
+            <div id="collapse1" class="collapse" data-parent="#accordion_fazer">
+                <div class="card-body">
+                    'Lorem ipsum...'
+                </div>
+            </div>
+        </div>`
         
         z.innerHTML += formacao;
         tarefas.appendChild(z);
