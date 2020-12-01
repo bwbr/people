62b8ea8e6cd17eb8ddc029aae82d7ea58caf82e2
Thayane <thayanecarvalho3@gmail.com>
2020-12-01 07:57:34 -0300

    Resolvendo o problema do acordeão

--- Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
+++ Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
@@ -1,6 +1,8 @@
 var campos = document.querySelector('[data-form-formacao]');
 var tarefas = document.querySelector('#accordion_fazer');
 var $ = document.querySelector.bind(document);
+let i = 8;
+
 $('[data-form-btn]').addEventListener('click', (event) => {
     event.preventDefault();
     console.log('Fui clicado')
@@ -9,17 +11,17 @@ $('[data-form-btn]').addEventListener('click', (event) => {
         let formacao = 
         `<div class="card formacoes" data-formacao draggable="true">
             <div class="card-header">
-                <a class="card-link" data-toggle="collapse" href="#collapse1">
+                <a class="card-link" data-toggle="collapse" href="#collapse${i}">
                    ${campos.value}
                 </a>
             </div>
-            <div id="collapse1" class="collapse" data-parent="#accordion_fazer">
+            <div id="collapse${i}" class="collapse" data-parent="#accordion_fazer">
                 <div class="card-body">
                     'Lorem ipsum...'
                 </div>
             </div>
         </div>`
-        
+        i++;
         z.innerHTML += formacao;
         tarefas.appendChild(z);
 }}); 
