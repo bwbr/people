3edfb172736ec1c8632c085493f5bff4e450c4d9
Thayane <thayanecarvalho3@gmail.com>
2020-12-01 08:41:14 -0300

    Conseguindo arrastar os recém criados

--- Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
+++ Thayane/assets/JS/CriarFormacoes/CriarFormacoes.js
@@ -1,27 +1,28 @@
 var campos = document.querySelector('[data-form-formacao]');
 var tarefas = document.querySelector('#accordion_fazer');
 var $ = document.querySelector.bind(document);
+let retorno = "";
 let i = 8;
 
 $('[data-form-btn]').addEventListener('click', (event) => {
     event.preventDefault();
-    console.log('Fui clicado')
     var z = document.createElement('div');
+    z.className = 'card formacoes arrastar_js';
+    z.draggable = true;
     if(campos != null){
         let formacao = 
-        `<div class="card formacoes" data-formacao draggable="true">
-            <div class="card-header">
-                <a class="card-link" data-toggle="collapse" href="#collapse${i}">
-                   ${campos.value}
-                </a>
-            </div>
-            <div id="collapse${i}" class="collapse" data-parent="#accordion_fazer">
-                <div class="card-body">
-                    'Lorem ipsum...'
+            `<div class="card-header">
+                    <a class="card-link" data-toggle="collapse" href="#collapse${i}">
+                       ${campos.value}
+                    </a>
                 </div>
-            </div>
-        </div>`
+                <div id="collapse${i}" class="collapse" data-parent="#accordion_fazer">
+                    <div class="card-body">
+                        'Lorem ipsum...'
+                    </div>
+                </div>
+            </div>`
         i++;
-        z.innerHTML += formacao;
+        z.innerHTML += formacao;        
         tarefas.appendChild(z);
 }}); 
