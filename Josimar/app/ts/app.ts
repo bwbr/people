159900e87e244366ef661c078dab6957adc40428
Josimar <contat.js@gmail.com>
2020-12-14 12:10:38 -0300

    Atualizações realizadas no comentário
    -Cadastro de atividades em local Storage
    -Criação de form para edição dos campos
    -Correção de scripts como o badge (contagem de atividades cadastradas, drag and rop e progress em skills)

--- Josimar/app/ts/app.ts
+++ Josimar/app/ts/app.ts
@@ -4,6 +4,9 @@ const controller = new AtividadeController();
 
 controller.atualiza();
 
+// submete o form que adiciona atividades
 $('#form_activity_add').submit(controller.adiciona.bind(controller));
+// submete o form que edita atividades
 $('#form_activity_edit').submit(controller.edita.bind(controller));
+// limpa o form que adiciona atividades
 $('#clear_btn').click(controller.limpa.bind(controller));
